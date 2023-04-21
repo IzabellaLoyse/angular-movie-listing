1f43a4e4003eb237d43f74a6040aae0799d65830
Izabella Loyse <izabellaloyse13@gmail.com>
2023-04-20 21:28:56 -0300

    fix(app.module): defining the environment in the application's app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { HttpClientModule } from '@angular/common/http';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MoviesModule } from './modules/movies/movies.module';
@@ -9,7 +10,12 @@ import { MoviesModule } from './modules/movies/movies.module';
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule, MoviesModule],
-  providers: [],
+  providers: [
+    {
+      provide: 'API_URL',
+      useValue: environment.apiUrl,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
