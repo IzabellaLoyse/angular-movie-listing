2469e971eb9f893be1b2d700b6d9c7156b5bd7b8
Izabella Loyse <izabellaloyse13@gmail.com>
2023-04-20 21:38:21 -0300

    fix(environment): changed api url to use https

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ const apiKey = 'c5c97de3';
 export const environment = {
   production: false,
   apiKey: 'c5c97de3',
-  apiUrl: `http://www.omdbapi.com/?apikey=${apiKey}&`,
+  apiUrl: `https://www.omdbapi.com/?apikey=${apiKey}&`,
 };
